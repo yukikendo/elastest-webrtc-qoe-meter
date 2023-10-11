d6600f738d06f1d7a692dba8cc39bce7fd2636ce
yukikendo <y.kendo@net.lab.uec.ac.jp>
2023-10-11 17:05:43 +0900

    コードの変更

--- scripts/apprtc.sh
+++ scripts/apprtc.sh
@@ -3,6 +3,7 @@
 #apprtcのディレクトリに書き換えて使ってください
 cd /home/ohzahata-qoe/Documents/GitHub/apprtc
 
-sudo docker build . --tag apprtc:latest
-
-sudo docker run -p 443:443 -p 8080:8080 -p 8089:8089 --rm -ti apprtc:latest
+#sudo docker build . --tag apprtc:latest
+docker build . --tag apprtc:latest
+#sudo docker run -p 443:443 -p 8080:8080 -p 8089:8089 --rm -ti apprtc:latest
+docker run -p 443:443 -p 8080:8080 -p 8089:8089 --rm -ti apprtc:latest
